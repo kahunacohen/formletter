642df88a75a14f65ca089aaebdf0d93fee046dd6
Aaron Cohen <acohen@mountainpasstech.com>
2022-01-09 22:17:49 +0200

    add foreign keys

--- formletter/admin.py
+++ formletter/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Template
+from .models import Letter, Template
 
 class TemplateAdmin(admin.ModelAdmin):
     fields = ['title', 'text',]
 
 admin.site.register(Template, TemplateAdmin)
+
+class LetterAdmin(admin.ModelAdmin):
+    fields = ['title', 'template',]
+
+admin.site.register(Letter, LetterAdmin)
--- formletter/models.py
+++ formletter/models.py
@@ -2,8 +2,19 @@ from django.db import models
 
 class Template(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
   title = models.CharField(max_length=100)
   text = models.TextField(max_length=10000)
+
+  def __str__(self):
+    return self.title
+
+class Letter(models.Model):
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  title = models.CharField(max_length=100)
+  template = models.ForeignKey(Template, on_delete=models.CASCADE, related_name="template")
+  
 
   def __str__(self):
     return self.title
